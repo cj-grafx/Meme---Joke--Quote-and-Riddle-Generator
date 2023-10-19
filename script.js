49e18bb25660fafb943f1077951d725888e4e280
cj-grafx <praise01882168@gmail.com>
2023-10-19 16:28:27 +0100

    new

--- script.js
+++ script.js
@@ -101,6 +101,19 @@ const memes = [
   "https://uploads-ssl.webflow.com/5f3c19f18169b62a0d0bf387/60d33bf45ccef72a83a30822_1RQn8ZpWk8JFIHiBCjSGNTBsSxHG3KMnU54Qf8khHOJwBmwOZMt5Q5uhLa-pwhVPrhQJwe3NS_gkWCpLFCPxFZdfUQCghZJIFLcZwvGMq3BYauR7RDf0xRmLqT9i4VXoTtBZNS5y.png",
   "https://uploads-ssl.webflow.com/5f3c19f18169b62a0d0bf387/60d33bf00f8de3388923e46e_pWDSAyLl1anr-yCxObb-b-MUilkQM0K2eER4_gwbN48qaP3NB4ZPl_P5C7CwKEQvC1fsQvHTsWd7yCMLixJ9v8rweKd9pBYGg6AVx67kohYbSpQiVIUz3svyG1s58XeqM7cKnq3x.png",
   "https://uploads-ssl.webflow.com/5f3c19f18169b62a0d0bf387/60d33bf1cf4ba77ae423d175_zwwc_oN3Ui8Z9CemAU9xH9T4cVHvKQduE8pKaVlXuPGwatLb4QHv2DTnx2XXKrlW0mxuXTY-89EUjk3bpxkIoz9LxH4mpgW8n6_sDq1TTEqfHWscELlm16JyhTfPgtBgT76Vf0gC.png",
+  "https://i.redd.it/a0v87gwzoge61.jpg",
+  "https://i.redd.it/q29egav34ee61.jpg",
+  "https://i.redd.it/iij16swxjie61.jpg",
+  "https://i.redd.it/vek7dm2hrge61.jpg",
+  "https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-8.png",
+  "https://miro.medium.com/max/1000/0*Ua695vjzFHV6VNOX.png",
+  "https://pbs.twimg.com/media/EKkPagPXkAA__Qo.jpg",
+  "https://code-love.com/wp-content/uploads/2019/03/download.jpeg",
+  "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Programmer-while-sleeping.jpg",
+  "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Evolution-of-Memory-Storage-1024x996.jpg",
+  "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Error-in-Code-896x1024.jpg",
+  "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Coding-Meme-Code-Comments-be-Like-925x1024.jpg",
+  "https://www.thecoderpedia.com/wp-content/uploads/2020/06/Internet-Explorer-Joke-915x1024.jpg",
 ];
 
 const jokes = [
